2b2ba77dd688a4c657b41e5339a67aa0a19906f1
HanBitPix <hanjt517@gmail.com>
2018-05-03 13:17:31 -0700

    corrected mlab login info

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 if(process.env.NODE_ENV === 'production'){
   module.exports = {mongoURI:
-    'mongodb://admin:hanone123@ds049568.mlab.com:49568/trachyourtech'}
+    'mongodb://admin:password@ds049568.mlab.com:49568/trachyourtech'}
 } else{
   module.exports = {mongoURI: 'mongodb://localhost/fullstack'}
 }
